edfe3406fbfbc7d56990e79ce4289df5d4a1b7ff
James Newell <james.newell@gmail.com>
2015-10-11 22:47:07 -0400

    apply -s changes to branch

--- DiskArbitrationAgent/DiskArbitrationAgent.m
+++ DiskArbitrationAgent/DiskArbitrationAgent.m
@@ -2,14 +2,14 @@
  * Copyright (c) 1998-2013 Apple Inc. All rights reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -17,15 +17,35 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
 #include "DAAgent.h"
 #include "DADialog.h"
 
+#include <libgen.h>
+#include <sysexits.h>
 #include <xpc/xpc.h>
 #include <DiskArbitration/DiskArbitrationPrivate.h>
+#include <IOKit/storage/IOStorageProtocolCharacteristics.h>
+
+static Boolean __gDAOptionSataNoMsg = FALSE;
+
+char * gDAProcessName = NULL;
+
+static void __usage( void )
+{
+    /*
+     * Print usage.
+     */
+
+    fprintf( stderr, "%s: [-s]\n", gDAProcessName );
+    fprintf( stderr, "options:\n" );
+    fprintf( stderr, "\t-d\tdisable messages for unreadble SATA disks\n" );
+
+    exit( EX_USAGE );
+}
 
 static void __DAAgentMessageCallback( xpc_object_t object );
 
@@ -90,6 +110,29 @@ static void __DAAgentMessageCallback( xpc_object_t object )
                             }
                             case _kDAAgentActionShowDeviceUnreadable:
                             {
+                                /*
+                                 * check if we should ignore unreadable SATA disks
+                                 */
+
+                                if ( __gDAOptionSataNoMsg ) {
+
+                                    CFDictionaryRef description;
+                                    CFTypeRef object;
+
+                                    description = DADiskCopyDescription( disk );
+
+                                    if ( description ) {
+                                        object = CFDictionaryGetValue( description, kDADiskDescriptionDeviceProtocolKey );
+                                        if ( object && CFEqual( object, CFSTR( kIOPropertyPhysicalInterconnectTypeSerialATA ) ) )
+                                        {
+                                            CFRelease(description);
+                                            break;
+                                        }
+
+                                        CFRelease(description);
+                                    }
+                                }
+
                                 DADialogShowDeviceUnreadable( disk );
 
                                 break;
@@ -114,8 +157,35 @@ static void __DAAgentMessageCallback( xpc_object_t object )
     }
 }
 
-int main( )
+int main( int argc, char * argv[], char * envp[] )
 {
+    char option;
+
+    gDAProcessName = basename( argv[0] );
+
+    /*
+     * Process arguments.
+     */
+
+    while ( ( option = getopt( argc, argv, "s" ) ) != -1 )
+    {
+        switch ( option )
+        {
+            case 's':
+            {
+                __gDAOptionSataNoMsg = TRUE;
+
+                break;
+            }
+            default:
+            {
+                __usage( );
+
+                break;
+            }
+        }
+    }
+
     xpc_connection_t connection;
 
     connection = xpc_connection_create_mach_service( _kDAAgentName, NULL, XPC_CONNECTION_MACH_SERVICE_LISTENER );
